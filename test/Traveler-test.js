0d933f5f3bdf986c49548be4964bd9359459614b
Kristy Nguyen <knyen008@gmail.com>
2022-06-08 12:58:36 -0600

    Create properties of and test for traveler id, name, and traveler type

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -3,16 +3,34 @@ import Traveler from "../src/Traveler";
 import { travelers } from "../src/data/travelers-data";
 
 describe('Traveler', () => {
-  let traveler1, traveler7;
+  let traveler1, traveler2;
 
   beforeEach(() => {
-    traveler1 = travelers[0];
-    traveler7 = travelers[6];
+    traveler1 = new Traveler(travelers[0]);
+    traveler2 = new Traveler(travelers[1]);
   });
 
   it('should be a function', function() {
     expect(Traveler).to.be.a('function');
   });
 
+  it('should be an instance of Traveler', () => {
+    expect(traveler1).to.be.an.instanceOf(Traveler);
+    expect(traveler2).to.be.an.instanceOf(Traveler);
+  });
 
+  it('should have an id', () => {
+    expect(traveler1.id).to.equal(1);
+    expect(traveler2.id).to.equal(2);
+  });
+
+  it('should have a name', () => {
+    expect(traveler1.name).to.equal('Ham Leadbeater');
+    expect(traveler2.name).to.equal('Rachael Vaughten');
+  });
+
+  it('should have a type of traveler', () => {
+    expect(traveler1.travelerType).to.equal('relaxer');
+    expect(traveler2.travelerType).to.equal('thrill-seeker');
+  });
 });
