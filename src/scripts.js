6daaa420df46c272b9d7a5e90a9fed4d97e6b97b
Kristy Nguyen <knyen008@gmail.com>
2022-06-10 09:58:47 -0600

    Create load data and render data fns to utilize API data in project

--- src/scripts.js
+++ src/scripts.js
@@ -18,13 +18,18 @@ window.addEventListener("load", (event) => {
 
 //Functions
 const loadData = () => {
-  getAll()
-    .then(data => {
-      const [travelerData, tripData, destinationData] = data;
-      const randomTravelerObj = travelerData.travelers[Math.floor(Math.random() * travelerData.travelers.length)];
-      const randomTraveler = new Traveler(randomTravelerObj);
-      return {randomTraveler}
+  getAll().then(data => {
+    travelerData = data[0];
+    tripData = data[1];
+    destinationData = data[2];
+    renderData();
     })
-    .then(({randomTraveler}) => {console.log(randomTraveler)})
     .catch((error) => console.log(`There has been an error! ${error}`));
+};
+
+const renderData = () => {
+  const randomTravelerObj = travelerData.travelers[Math.floor(Math.random() * travelerData.travelers.length)];
+  const randomTraveler = new Traveler(randomTravelerObj);
+  const trips = tripData.trips;
+  const destinations = destinationData.destinations;
 };
