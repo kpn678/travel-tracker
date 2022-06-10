86f3a38937a73e69f18eb818741ed9aad598a07f
Kristy Nguyen <knyen008@gmail.com>
2022-06-10 15:13:05 -0600

    Create grid and hide/show hidden grid pages

--- src/scripts.js
+++ src/scripts.js
@@ -9,11 +9,16 @@ import Traveler from "./Traveler";
 let travelerData, tripData, destinationData;
 
 //Query Selectors
-
-//Event Listeners
-window.addEventListener("load", (event) => {
-  loadData();
-});
+const homeButton = document.querySelector('.home-button');
+const pastButton = document.querySelector('.past-trips-button');
+const presentButton = document.querySelector('.present-trips-button');
+const futureButton = document.querySelector('.future-trips-button');
+const pendingButton = document.querySelector('.pending-trips-button');
+const homePage = document.querySelector('.home-page');
+const pastPage = document.querySelector('.past-page');
+const presentPage = document.querySelector('.present-page');
+const futurePage = document.querySelector('.future-page');
+const pendingPage = document.querySelector('.pending-page');
 
 //Functions
 const loadData = () => {
@@ -32,3 +37,59 @@ const renderData = () => {
   const trips = tripData.trips;
   const destinations = destinationData.destinations;
 };
+
+const show = (element) => {
+  element.classList.remove('hidden');
+};
+
+const hide = (element) => {
+  element.classList.add('hidden');
+};
+
+const displayHome = () => {
+  show(homePage);
+  hide(pastPage);
+  hide(presentPage);
+  hide(futurePage);
+  hide(pendingPage);
+};
+
+const displayPast = () => {
+  hide(homePage);
+  show(pastPage);
+  hide(presentPage);
+  hide(futurePage);
+  hide(pendingPage);
+};
+
+const displayPresent = () => {
+  hide(homePage);
+  hide(pastPage);
+  show(presentPage);
+  hide(futurePage);
+  hide(pendingPage);
+};
+
+const displayFuture = () => {
+  hide(homePage);
+  hide(pastPage);
+  hide(presentPage);
+  show(futurePage);
+  hide(pendingPage);
+};
+
+const displayPending = () => {
+  hide(homePage);
+  hide(pastPage);
+  hide(presentPage);
+  hide(futurePage);
+  show(pendingPage);
+};
+
+//Event Listeners
+window.addEventListener("load", loadData);
+homeButton.addEventListener('click', displayHome);
+pastButton.addEventListener('click', displayPast);
+presentButton.addEventListener('click', displayPresent);
+futureButton.addEventListener('click', displayFuture);
+pendingButton.addEventListener('click', displayPending);
